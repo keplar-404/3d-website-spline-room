7c3f0c04b8085241b0c6f87855ce0316969525f3
keplar <106798855+keplar-404@users.noreply.github.com>
2023-02-14 23:04:28 +0600

    updated

--- src/components/SplineScene.js
+++ src/components/SplineScene.js
@@ -3,13 +3,8 @@ import styled from "styled-components";
 export default function App() {
   return (
     <Wrapper>
-
-      <Spline
-        className="spline"
-        scene="https://prod.spline.design/2RGptqpHPuG1FpgT/scene.splinecode"
-        />
+     <Spline  className="spline" scene="https://prod.spline.design/2RGptqpHPuG1FpgT/scene.splinecode" />
    </Wrapper>
-
   );
 }
 
